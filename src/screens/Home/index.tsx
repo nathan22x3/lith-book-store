4101975ddece01f8095af8c064f74628d205e45e
Nathan Nguyen <67423224+nathan22x3@users.noreply.github.com>
2022-02-03 08:49:57 +0700

    chore: temporary fix nested list inside scrollview

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,8 +2,7 @@
 import { Box, Container, IconBell, IconChevronRight, Text, TextInput } from '@components/index';
 import { BookCard, COVER_WIDTH, ReadingProgress } from '@screens/Home/components';
 import { IS_ANDROID, TAB_BAR_HEIGHT } from '@utils/const';
-import { FlatList, TouchableOpacity } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { FlatList, ScrollView, TouchableOpacity } from 'react-native';
 import { LIBRARY_BOOKS } from './mock';
 
 const OFFSET = 20;
@@ -19,7 +18,7 @@ const Home = () => {
           <IconBell />
         </TouchableOpacity>
       </Box>
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Box mt="xs">
           <Text variant="title" mx="l" mb="m">
             From Your Library
@@ -34,35 +33,30 @@ const Home = () => {
             keyExtractor={({ id }) => id}
             horizontal
             showsHorizontalScrollIndicator={false}
-            ListFooterComponent={() => <Box width={OFFSET} />}
+            ListFooterComponent={<Box width={OFFSET} />}
             snapToOffsets={LIBRARY_BOOKS.map((_, index) => (index + 1) * (COVER_WIDTH + OFFSET))}
             decelerationRate={IS_ANDROID ? 0.9 : 'fast'}
           />
-          <FlatList
-            data={LIBRARY_BOOKS}
-            renderItem={({ item: { title, cover, genre, rating, overview } }) => (
-              <BookCard {...{ title, cover, genre, rating, overview }} />
-            )}
-            keyExtractor={({ id }) => id}
-            ListHeaderComponent={
-              <TouchableOpacity>
-                <Box
-                  flexDirection="row"
-                  justifyContent="space-between"
-                  alignItems="center"
-                  px="l"
-                  my="m"
-                >
-                  <Text variant="headline2">Top page turners</Text>
-                  <IconChevronRight />
-                </Box>
-              </TouchableOpacity>
-            }
-            ItemSeparatorComponent={() => <Box height={OFFSET} />}
-            ListFooterComponent={() => <Box height={TAB_BAR_HEIGHT + OFFSET * 2} />}
-            showsVerticalScrollIndicator={false}
-            decelerationRate={IS_ANDROID ? 0.9 : 'fast'}
-          />
+          <TouchableOpacity>
+            <Box
+              flexDirection="row"
+              justifyContent="space-between"
+              alignItems="center"
+              px="l"
+              my="m"
+            >
+              <Text variant="headline2">Top page turners</Text>
+              <IconChevronRight />
+            </Box>
+          </TouchableOpacity>
+          <Box>
+            {LIBRARY_BOOKS.map(({ id, title, cover, genre, rating, overview }, index, list) => (
+              <Box key={id}>
+                <BookCard {...{ title, cover, genre, rating, overview }} />
+                <Box height={index === list.length - 1 ? TAB_BAR_HEIGHT + OFFSET * 2 : OFFSET} />
+              </Box>
+            ))}
+          </Box>
         </Box>
       </ScrollView>
     </Container>
